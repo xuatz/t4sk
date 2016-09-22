8e3563d0e5cd42cd97cebf3b1130c9ed69d72de8
xuatz <xzlow10@gmail.com>
2016-09-22 21:04:50 +0800

    added parse package, plus some dummy db api + test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import App from './containers/app';
 
 import './index.css';
 
+var APP_ID = "x02THLMuG7o1RNqFKQ4ZmfeAstAHt6r7ejSFOeDq";
+var JAVASCRIPT_KEY = "BCK3GSK3auSwpmI28WMdr1yxEEJXInmYlcDzxYpy";
+Parse.initialize(APP_ID, JAVASCRIPT_KEY);
+Parse.serverURL = 'https://parseapi.back4app.com';
+
+//====================
+
 const store = configureStore();
 
 render((
