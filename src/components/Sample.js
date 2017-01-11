1c994f6cca8a9a4b2ac7fe3cf8d2edddf196631f
xuatz <xzlow10@gmail.com>
2017-01-11 20:33:44 +0800

    *WIP* sample

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,10 +1,28 @@
 import React from 'react'
 
 const Sample = () => {
+	let completedTasks = ["completed1", "completed2", 'compelted3']
+
 	return (
 		<div>
-			<section>
+			<p>
+				*This is a mockup for a daily report screen*
+			</p>
+
+			<section style={{
+				background: 'red'
+			}} >
 				Tasks completed
+
+				<div style={{
+					padding: '5px 20px'
+				}} >
+					{completedTasks.map((item, index) => 
+						<div>
+							{item}
+						</div>
+					)}
+				</div>
 			</section>
 			<section>
 				Tasks added today
